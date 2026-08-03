c1e72767506c798faca94dbf450af9531ef7af59
SakuraCake <2135484803@qq.com>
2026-08-03 18:52:32 +0800

    refactor(core): remove deprecated ServiceRegistry DI wrapper

--- lib/core/di/service_registry.dart
+++
@@ -1,7 +0,0 @@
-/// DI container abstraction. Default implementation wraps GetX.
-@Deprecated('Use BiliBridge.register() (GetX DI) directly instead.')
-abstract class ServiceRegistry {
-  void bind<T>(T Function() factory);
-  T resolve<T>();
-  bool isRegistered<T>();
-}
