7da4d18f8b23b9bbe956e8a1da777bd4f88d80d8
SakuraCake <2135484803@qq.com>
2026-08-25 21:53:59 +0800

    refactor(getx): convert Get.find<AccountProvider> → appRead in vote.dart

--- lib/pages/dynamics/widgets/vote.dart
+++ lib/pages/dynamics/widgets/vote.dart
@@ -21,7 +21,6 @@ import 'package:skf/utils/num_utils.dart';
 import 'package:skf/utils/platform_utils.dart';
 import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 
 class VotePanel extends StatefulWidget {
   final CoreVoteInfo voteInfo;
@@ -49,7 +48,7 @@ class _VotePanelState extends State<VotePanel> {
       _voteInfo.endTime! * 1000 > DateTime.now().millisecondsSinceEpoch;
   late bool _showPercentage = !_enabled;
   late final _maxCnt = _voteInfo.choiceCnt ?? _voteInfo.options.length;
-  final isLogin = Get.find<AccountProvider>().isLogin;
+  late final isLogin = appRead(accountProvider).isLogin;
   late Listenable followeeVote;
 
   @override
